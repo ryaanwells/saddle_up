f97f5c9b8a08e71426363ee7a8874dea26685cbc
Ryan Wells <ryaanwells@googlemail.com>
2015-02-16 23:59:13 +0000

    Made it pretty(ier)

--- saddle_up.js
+++ saddle_up.js
@@ -37,9 +37,11 @@ function main_ctl_fn($scope, $http, $q){
     $scope.baseUrl = baseUri;
   }
 
+
   function updateFullUrl(){
     $scope.fullUrl = $scope.raml_doc.baseUri;
     var partial_string = "";
+
     angular.forEach($scope.baseUriParameters, function(value, key){
       if (value.hasOwnProperty("value") && value.value){
         $scope.fullUrl = formatStringPartial($scope.fullUrl, key, value.value);
@@ -48,6 +50,7 @@ function main_ctl_fn($scope, $http, $q){
 
     angular.forEach($scope.raml_url.slice(1), function(partial){
       if (partial.type == "Method") { return; }
+
       partial_string = partial.relativeUri;
       if (partial.hasOwnProperty("uriParameters")){
         angular.forEach(partial.uriParameters, function(value, key){
@@ -89,9 +92,34 @@ function main_ctl_fn($scope, $http, $q){
   }
   $scope.getSegmentOptions = getSegmentOptions;
 
+  function extendAndInitialize(dest, src){
+    angular.forEach(src, function(value, key){
+      if (value.enum && !value.value){
+        value.value = value.enum[0];
+      }
+      this[key] = value
+    }, dest);
+  }
+
+  function updateParameterSelection(){
+    var queryElement = $scope.raml_url[$scope.raml_url.length - 1];
+    if (queryElement.hasOwnProperty("queryParameters")){
+      extendAndInitialize($scope.queryParameters, queryElement.queryParameters);
+    }
+
+    angular.forEach($scope.raml_url, function(partial, index){
+      if (partial.hasOwnProperty("baseUriParameters")){
+        extendAndInitialize($scope.baseUriParameters, partial.baseUriParameters);
+      }
+      if (partial.hasOwnProperty("uriParameters")){
+        extendAndInitialize($scope.uriParameters, partial.uriParameters);
+      }
+    });
+
+  }
+
   function ramlUrlUpdate(newRamlUrl, oldRamlUrl, event){
     var oldRamlPartial = null;
-    var queryElement = {};
     var currentElement = {};
     var new_name = "";
     var old_name = "";
@@ -109,21 +137,67 @@ function main_ctl_fn($scope, $http, $q){
       }
     });
 
-    queryElement = $scope.raml_url[$scope.raml_url.length - 1];
-    if (queryElement.hasOwnProperty("queryParameters")){
-      $scope.queryParameters = queryElement.queryParameters;
-    }
-
-    angular.forEach($scope.raml_url, function(partial, index){
-      if (partial.hasOwnProperty("baseUriParameters")){
-        angular.extend($scope.baseUriParameters, partial.baseUriParameters);
-      }
-      if (partial.hasOwnProperty("uriParameters")){
-        angular.extend($scope.uriParameters, partial.uriParameters);
-      }
-    });
+    updateParameterSelection();
     updateFullUrl();
   }
+
+  function validateType(parameter){
+    var typeMappings = {
+      "string": angular.isString,
+      "number": angular.isNumber,
+      "integer": function(value){
+        // polyfil from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+        return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
+       },
+      "date": angular.isDate,
+      "boolean": function(value){ return typeof value === "boolean"}
+    };
+    return typeMappings[parameter.type](parameter.value);
+  }
+
+  function validateRequest(){
+    var isValid = true;
+    var paramGroups = [$scope.baseUriParameters,
+                       $scope.uriParameters,
+                       $scope.queryParameters];
+
+    if ($scope.raml_url.length === 0 ||
+        $scope.raml_url[$scope.raml_url.length - 1].type !== "Method"){
+      isValid = false;
+    }
+
+    angular.forEach(paramGroups, function(paramGroup){
+      angular.forEach(paramGroup, function(parameter, key){
+        if ( parameter.required &&
+             !( parameter.value || parameter.default) &&
+             !validateType(parameter)){
+          isValid = false;
+        }
+      });
+    });
+    return isValid;
+  }
+  $scope.validateRequest = validateRequest;
+
+  function projectParameters(parameterObject){
+    var projectedParameters = {};
+    angular.forEach(parameterObject, function(value, key){
+      this[key] = value.value;
+    }, projectedParameters);
+    return projectedParameters;
+  }
+
+  function makeRequest(){
+    if (validateRequest()){
+      var request = {};
+      request["url"] = $scope.fullUrl;
+      request["method"] = $scope.raml_url[$scope.raml_url.length - 1].method;
+      request["data"] = projectParameters($scope.queryParameters);
+      console.log(request);
+      $http(request).then(logResponse, logResponse);
+    }
+  }
+  $scope.makeRequest = makeRequest;
 
   function processRAML(raml){
     $scope.raml_doc = raml;
