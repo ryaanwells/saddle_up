785fe8c9610ac868ab7830c017e6bcbac35f6fda
Ryan Wells <ryaanwells@googlemail.com>
2015-05-15 21:17:06 +0100

    Added in a 'last' method to arrays, and implemented an autocomplete on options with only one item

--- saddle_up.js
+++ saddle_up.js
@@ -1,10 +1,72 @@
 
 var saddle_up = angular.module("saddle_up", ["ui.bootstrap"]);
 
-function saddle_up_ctl($scope, $http, $q){
+function utils(){
 
-  $scope.raml_doc = {};
-  $scope.raml_url = [];
+  Object.defineProperty(Array.prototype, 'last', {
+    enumerable: false,
+    configurable: true,
+    get: function() {
+      if (this.length > 0){
+        return this[this.length - 1];
+      }
+      return undefined;
+    },
+    set: undefined
+  });
+
+  function formatString(input, key, value){
+    var regexp_base = "{" + key + "}";
+    return input.replace(new RegExp(regexp_base, 'g'), value);
+  }
+
+  function extendAndInitialize(dest, src){
+    src = src || {};
+    angular.forEach(src, function(value, key){
+      if (value.enum && !value.value){
+        value.value = value.enum[0];
+      }
+      this[key] = value
+    }, dest);
+  }
+
+  function project(source, attr){
+    var projected = {};
+    angular.forEach(source, function(value, key){
+      this[key] = value[attr];
+    }, projected);
+    console.log(projected);
+    return projected;
+  }
+
+  function validateType(parameter){
+    var typeMappings = {
+      "string": angular.isString,
+      "number": angular.isNumber,
+      "integer": function(value){
+        // polyfil from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
+        return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
+      },
+      "date": angular.isDate,
+      "boolean": function(value){ return typeof value === "boolean"}
+    };
+    return typeMappings[parameter.type](parameter.value);
+  }
+
+  return {
+    formatString: formatString,
+    extendAndInitialize: extendAndInitialize,
+    project: project,
+    validateType: validateType
+  };
+}
+
+saddle_up.factory('utils', utils);
+
+function saddle_up_ctl($scope, $http, $q, utils){
+
+  $scope.ramlDoc = {};
+  $scope.ramlUrl = [];
 
   $scope.baseUriParameters = {};
   $scope.uriParameters = {};
@@ -16,33 +78,28 @@ function saddle_up_ctl($scope, $http, $q){
   $scope.response = {};
 
 
-  function formatStringPartial(input_string, key, value){
-    var regexp_base = "{" + key + "}";
-    return input_string.replace(new RegExp(regexp_base, 'g'), value);
-  }
-
   function updateFullUrl(){
-    $scope.fullUrl = $scope.raml_doc.baseUri;
-    var partial_string = "";
+    $scope.fullUrl = $scope.ramlDoc.baseUri;
+    var partial_uri = "";
 
     angular.forEach($scope.baseUriParameters, function(value, key){
       if (value.hasOwnProperty("value") && value.value){
-        $scope.fullUrl = formatStringPartial($scope.fullUrl, key, value.value);
+        $scope.fullUrl = utils.formatString($scope.fullUrl, key, value.value);
       }
     });
 
-    angular.forEach($scope.raml_url.slice(1), function(partial){
-      if (partial.type == "Method") { return; }
-
-      partial_string = partial.relativeUri;
-      if (partial.hasOwnProperty("uriParameters")){
-        angular.forEach(partial.uriParameters, function(value, key){
-          if (value.hasOwnProperty("value") && value.value){
-            partial_string = formatStringPartial(partial_string, key, value.value);
-          }
-        });
+    angular.forEach($scope.ramlUrl.slice(1), function(partial){
+      if (partial.type !== "Method") {
+        partial_uri = partial.relativeUri;
+        if (partial.hasOwnProperty("uriParameters")){
+          angular.forEach(partial.uriParameters, function(value, key){
+            if (value.hasOwnProperty("value") && value.value){
+              partial_uri = utils.formatString(partial_uri, key, value.value);
+            }
+          });
+        }
+        $scope.fullUrl = $scope.fullUrl + partial_uri;
       }
-      $scope.fullUrl = $scope.fullUrl + partial_string;
     });
   }
 
@@ -75,60 +132,44 @@ function saddle_up_ctl($scope, $http, $q){
   }
   $scope.getSegmentOptions = getSegmentOptions;
 
-  function extendAndInitialize(dest, src){
-    angular.forEach(src, function(value, key){
-      if (value.enum && !value.value){
-        value.value = value.enum[0];
-      }
-      this[key] = value
-    }, dest);
-  }
+
 
   function updateParameterSelection(){
-    var queryElement = $scope.raml_url[$scope.raml_url.length - 1];
-    if (queryElement.hasOwnProperty("queryParameters")){
-      extendAndInitialize($scope.queryParameters, queryElement.queryParameters);
-    }
-
-    angular.forEach($scope.raml_url, function(partial, index){
-      if (partial.hasOwnProperty("baseUriParameters")){
-        extendAndInitialize($scope.baseUriParameters, partial.baseUriParameters);
-      }
-      if (partial.hasOwnProperty("uriParameters")){
-        extendAndInitialize($scope.uriParameters, partial.uriParameters);
-      }
-    });
-
-  }
-
-  function ramlUrlUpdate(index){
     $scope.baseUriParameters = {};
     $scope.uriParameters = {};
     $scope.queryParameters = {};
 
-    if ( index < $scope.raml_url.length - 1 ){
-      $scope.raml_url = $scope.raml_url.slice(0, index + 1);
+    angular.forEach($scope.ramlUrl, function(partial, index){
+      utils.extendAndInitialize($scope.baseUriParameters, partial.baseUriParameters);
+      utils.extendAndInitialize($scope.uriParameters, partial.uriParameters);
+      utils.extendAndInitialize($scope.queryParameters, partial.queryParameters);
+    });
+
+  }
+
+  function printout(){
+    console.log($scope.ramlUrl);
+  }
+  $scope.printout = printout;
+
+  function ramlUrlUpdate(index){
+    if ( index < $scope.ramlUrl.length - 1 ){
+      // Trim the URL if we change a middle segment.
+      $scope.ramlUrl = $scope.ramlUrl.slice(0, index + 1);
+    }
+
+    while (getSegmentOptions($scope.ramlUrl.last).length === 1){
+      // Autocomplete the next segments if there is only one option.
+      $scope.ramlUrl.push($scope.ramlUrl.last.options[0].option);
     }
 
     updateParameterSelection();
     updateFullUrl();
-    $scope.endpointDescription = $scope.raml_url[$scope.raml_url.length - 1].description || "";
+    $scope.endpointDescription = $scope.ramlUrl.last.description;
   }
   $scope.ramlUrlUpdate = ramlUrlUpdate;
 
-  function validateType(parameter){
-    var typeMappings = {
-      "string": angular.isString,
-      "number": angular.isNumber,
-      "integer": function(value){
-        // polyfil from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
-        return typeof value === "number" && isFinite(value) && Math.floor(value) === value;
-       },
-      "date": angular.isDate,
-      "boolean": function(value){ return typeof value === "boolean"}
-    };
-    return typeMappings[parameter.type](parameter.value);
-  }
+
 
   function validateRequest(){
     var isValid = true;
@@ -136,8 +177,7 @@ function saddle_up_ctl($scope, $http, $q){
                        $scope.uriParameters,
                        $scope.queryParameters];
 
-    if ($scope.raml_url.length === 0 ||
-        $scope.raml_url[$scope.raml_url.length - 1].type !== "Method"){
+    if ($scope.ramlUrl.length === 0 || $scope.ramlUrl.last.type !== "Method"){
       isValid = false;
     }
 
@@ -145,7 +185,7 @@ function saddle_up_ctl($scope, $http, $q){
       angular.forEach(paramGroup, function(parameter, key){
         if ( parameter.required &&
              !( parameter.value || parameter.default) &&
-             !validateType(parameter)){
+             !utils.validateType(parameter)){
           isValid = false;
         }
       });
@@ -154,13 +194,7 @@ function saddle_up_ctl($scope, $http, $q){
   }
   $scope.validateRequest = validateRequest;
 
-  function projectParameters(parameterObject){
-    var projectedParameters = {};
-    angular.forEach(parameterObject, function(value, key){
-      this[key] = value.value;
-    }, projectedParameters);
-    return projectedParameters;
-  }
+
 
   function success(response){
     $scope.response = response.data;
@@ -174,8 +208,8 @@ function saddle_up_ctl($scope, $http, $q){
     if (validateRequest()){
       var request = {
         "url": $scope.fullUrl,
-        "method": $scope.raml_url[$scope.raml_url.length - 1].method,
-        "data": projectParameters($scope.queryParameters)
+        "method": $scope.ramlUrl.last.method,
+        "data": utils.project($scope.queryParameters, 'value')
       };
       if (request.method.toUpperCase() === "GET") { request["params"] = request.data; }
 
@@ -186,10 +220,10 @@ function saddle_up_ctl($scope, $http, $q){
   $scope.makeRequest = makeRequest;
 
   function processRAML(raml){
-    $scope.raml_doc = raml;
+    $scope.ramlDoc = raml;
     $scope.baseUrl = raml.baseUri
     $scope.baseUriParameters = raml.baseUriParameters;
-    $scope.raml_url[0] = $scope.raml_doc;
+    $scope.ramlUrl[0] = $scope.ramlDoc;
     $scope.$watch("baseUriParameters", updateFullUrl, true);
     $scope.$watch("uriParameters", updateFullUrl, true);
     updateParameterSelection();
@@ -205,5 +239,5 @@ function saddle_up_ctl($scope, $http, $q){
 
 };
 
-saddle_up_ctl.$inject = ["$scope", "$http", "$q"];
+saddle_up_ctl.$inject = ["$scope", "$http", "$q", "utils"];
 saddle_up.controller("saddle_up_ctl", saddle_up_ctl);
